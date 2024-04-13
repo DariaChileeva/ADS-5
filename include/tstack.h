53b2f6e7c85813a4a8ca52dc3929c268f07e219c
DariaChileeva <dariachileeva@yandex.ru>
2024-04-13 19:08:26 +0300

    ads5

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,33 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    static const int Size = size;
+    T data[Size];
+    int top;
+public:
+    TStack() : top(0) {}
+    void push(const T &item) {
+        if (top <= size - 1)
+            data[top+1] = item;
+        else
+            throw std::string("Full stack!");
+    }
+    T pop() {
+        if (top > 0)
+            return data[--top];
+        else
+            throw std::string("Empty stack!");
+    }
+    bool isEmpty() {
+        return top == 0;
+    }
+    T get() {
+        if (!isEmpty())
+            return data [top - 1];
+        else
+            throw std::string("Empty!");
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
